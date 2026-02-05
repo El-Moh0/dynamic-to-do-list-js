fa0b4f41ebef8481108ff9fc63f16be9ac0e9a08
Alugbe Idris <mrmoh00700@gmail.com>
2026-02-05 23:42:48 +0100

    finished dynamic to do list

--- script.js
+++ script.js
@@ -3,10 +3,17 @@ document.addEventListener("DOMContentLoaded", function (){
     const addButton = document.getElementById("add-task-btn")
     const taskInput = document.getElementById("task-input")
     const taskList = document.getElementById("task-list")
+    
+    loadTasks()
+    
+    function loadTasks(){
+        const storedTasks = JSON.parse(localStorage.getItem("tasks") || "[]")
+        storedTasks.forEach(taskText => addTask(taskText, false))
+    }
 
-
-    function addTask(){
-        const taskText = taskInput.value.trim()
+    function addTask(taskText = null, save = true){
+        if (taskText === null)
+            taskText = taskInput.value.trim()
         if (taskText === ""){
             alert("Enter your tasks")
             return
@@ -24,10 +31,23 @@ document.addEventListener("DOMContentLoaded", function (){
         li.appendChild(removeButton)
         taskList.appendChild(li)
 
+        if (save){
+            const storedTasks=JSON.parse(localStorage.getItem("tasks") || "[]")
+            storedTasks.push(taskText)
+            localStorage.setItem("tasks", JSON.stringify(storedTasks))
+        }
+
         taskInput.value = ""
 
     }
 
+    function removeTaskFromStorage(taskText){
+    let storedTasks = JSON.parse(localStorage.getItem('tasks') || '[]')
+    storedTasks = storedTasks.filter(task => task !== task)
+    localStorage.setItem('tasks', JSON.stringify(storedTasks))    
+    }
+
+   
     addButton.addEventListener("click", addTask)
 taskInput.addEventListener("keypress", function(event){
     if (event.key === 'Enter'){
